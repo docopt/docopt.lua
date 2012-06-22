8a1a97c082cfa89c6a9dd387421ae665bff0cc13
James Graves <james.c.graves.jr@gmail.com>
2012-06-22 03:44:00 -0500

    Simple OO class function added.
    Adapted from: http://lua-users.org/wiki/SimpleLuaClasses

--- docopt.lua
+++ docopt.lua
@@ -1,4 +1,52 @@
 
+--- Simple OO system with inheritance.
+-- @param base (optional) Base class to inherit methods from.
+-- @param init Initialization function for the class.
+-- Compatible with Lua 5.1 (not 5.0).
+-- Adapted from: http://lua-users.org/wiki/SimpleLuaClasses
+local function class(base, init)
+    local c = {}    -- a new class instance
+    if not init and type(base) == 'function' then
+        init = base
+        base = nil
+    elseif type(base) == 'table' then
+        -- our new class is a shallow copy of the base class!
+        for i,v in pairs(base) do
+            c[i] = v
+        end
+        c._base = base
+    end
+    -- the class will be the metatable for all its objects,
+    -- and they will look up their methods in it.
+    c.__index = c
+
+    -- expose a constructor which can be called by <classname>(<args>)
+    local mt = {}
+    mt.__call = function(class_tbl, ...)
+        local obj = {}
+        setmetatable(obj,c)
+        if init then
+            init(obj,...)
+        else
+            -- make sure that any stuff from the base class is initialized!
+            if base and base.init then
+                base.init(obj, ...)
+            end
+        end
+        return obj
+    end
+    c.init = init
+    c.is_a = function(self, other_class)
+        local m = getmetatable(self)
+        while m do
+            if m == other_class then return true end
+            m = m._base
+        end
+        return false
+    end
+    setmetatable(c, mt)
+    return c
+end
 --- Process command-line arguments based on help string.
 -- docopt will parse the help message to see what the value usage cases
 -- are, and also to see the options.  Then it will parse the list of arguments
