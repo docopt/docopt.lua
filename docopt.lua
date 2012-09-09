85300d01eeab2d3cd1d50ef097964c50412b48fc
James Graves <james.c.graves.jr@gmail.com>
2012-09-09 06:44:26 -0500

    Partial Pattern class.

--- docopt.lua
+++ docopt.lua
@@ -3,14 +3,33 @@ local C     = require 'pl.comprehension' . new()
 local class = require 'pl.class'
 
 
+--
+-- Other utilities.
+--
 
+local function flatten(...)
+    local flat_list = {}
+    local flat_list_next = 1
+    for i = 1, select('#', ...) do
+        local t = select(i, ...)
+        for j = 1, #t do
+            flat_list[flat_list_next] = t[j]
+            flat_list_next = flat_list_next + 1
+        end
+    end
+end
+
+local Ether
+local Required
 --
 -- Pattern class.
 --
-local Pattern = class(  function(self, name, ...)
-                            self.name = assert(name)
-                            self.children = {...}
-                        end)
+local Pattern = class()
+
+function Pattern:_init(name, ...)
+    self.name = assert(name)
+    self.children = {...}
+end
 
 function Pattern:__eq(other)
     return __tostring(self) == __tostring(other)
@@ -23,6 +42,43 @@ function Pattern:__tostring()
     end
     return string.format("%s(%s)", self.name, children_repr)
 end
+
+function Pattern:flat()
+    if not self.children then return {self} end
+    return flatten(unpack(self.children))
+end
+
+function Pattern:fix()
+    self:fix_identities()
+    self:fix_list_arguments()
+    return self
+end
+
+--- Make pattern-tree tips point to the same object if they are equal.
+function Pattern:fix_identities(uniq)
+    if not self.children then
+        return self
+    end
+    uniq = uniq or set(self:flat())
+    for i, c in ipairs(self.children) do
+        if not c.children then
+            -- assert( c in uniq )
+            -- self.children[i] = uniq[uniq.index(c)]
+        else
+            c:fix_identities(uniq)
+        end
+    end
+end
+
+--- Find arguments that should accumulate values and fix them.
+function Pattern:fix_list_arguments()
+end
+
+--- Transform pattern into an equivalent, with only top-level Either.
+function Pattern:either()
+end
+
+
 --- Process command-line arguments based on help string.
 --
 -- docopt() will parse the help message to see what the value usage cases
@@ -50,4 +106,5 @@ end
 
 return {
     docopt = docopt,
+    Pattern = Pattern,
 }
